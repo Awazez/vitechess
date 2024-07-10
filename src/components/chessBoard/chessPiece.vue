728d05322658896f579ffb38bcee6fa2dac93791
cherbourg@fub.fr <cherbourg@fub.fr>
2024-07-10 23:12:29 +0200

    modification pour le changement d'architecture

--- src/components/chessBoard/chessPiece.vue
+++ src/components/chessBoard/chessPiece.vue
@@ -6,18 +6,18 @@
 <script>
 import { defineComponent } from 'vue';
 
-import bB from '../assets/pieces/cburnett/bB.svg';
-import bK from '../assets/pieces/cburnett/bK.svg';
-import bN from '../assets/pieces/cburnett/bN.svg';
-import bP from '../assets/pieces/cburnett/bP.svg';
-import bQ from '../assets/pieces/cburnett/bQ.svg';
-import bR from '../assets/pieces/cburnett/bR.svg';
-import wB from '../assets/pieces/cburnett/wB.svg';
-import wK from '../assets/pieces/cburnett/wK.svg';
-import wN from '../assets/pieces/cburnett/wN.svg';
-import wP from '../assets/pieces/cburnett/wP.svg';
-import wQ from '../assets/pieces/cburnett/wQ.svg';
-import wR from '../assets/pieces/cburnett/wR.svg';
+import bB from '../../assets/pieces/cburnett/bB.svg';
+import bK from '../../assets/pieces/cburnett/bK.svg';
+import bN from '../../assets/pieces/cburnett/bN.svg';
+import bP from '../../assets/pieces/cburnett/bP.svg';
+import bQ from '../../assets/pieces/cburnett/bQ.svg';
+import bR from '../../assets/pieces/cburnett/bR.svg';
+import wB from '../../assets/pieces/cburnett/wB.svg';
+import wK from '../../assets/pieces/cburnett/wK.svg';
+import wN from '../../assets/pieces/cburnett/wN.svg';
+import wP from '../../assets/pieces/cburnett/wP.svg';
+import wQ from '../../assets/pieces/cburnett/wQ.svg';
+import wR from '../../assets/pieces/cburnett/wR.svg';
 
 export default defineComponent({
   name: 'ChessPiece',
