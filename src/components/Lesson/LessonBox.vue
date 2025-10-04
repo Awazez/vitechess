d7acf304d1b72accc0adc9bb960315b6bbaf4284
Awazez <awazez@example.com>
2025-10-04 06:11:03 +0200

    Use flip-2.svg file instead of inline SVG

--- src/components/Lesson/LessonBox.vue
+++ src/components/Lesson/LessonBox.vue
@@ -10,12 +10,7 @@
           @click="$emit('flip-board')"
           :title="isEnglish ? 'Flip board' : 'Retourner l\'échiquier'"
         >
-          <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M3 7V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v2"/>
-            <path d="M3 17v2a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-2"/>
-            <path d="M8 12h8"/>
-            <path d="M12 8l4 4-4 4"/>
-          </svg>
+          <img src="/flip-2.svg" alt="Flip board" width="12" height="12" />
         </button>
       </div>
     </h2>
